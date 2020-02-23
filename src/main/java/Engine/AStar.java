3a5167c5551ff083ad91c5f99ac8e369201e0ac4
artg.dev <artg3.dev@gmail.com>
2020-02-23 10:47:12 -0500

    aded copyrights

--- src/main/java/Engine/AStar.java
+++ src/main/java/Engine/AStar.java
@@ -1,3 +1,12 @@
+
+/*------------------------------------------------------------------------------
+ @Author Art Garcia
+ Copyright (c) 2/2020
+ Project: AStarEngine
+ Last Modified: 2/23/20, 10:11 AM
+ License: GNU Lesser General Public License v3.0
+ -----------------------------------------------------------------------------*/
+
 package Engine;
 
 import java.util.ArrayList;
