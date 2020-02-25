85364919cf5032bddaaf546ba113d0047deec562
artg.dev <artg3.dev@gmail.com>
2020-02-25 17:39:46 -0500

    Create AStarFramework.java
    interface for defining the functions used by the AStarEngine

---
+++ src/main/java/Engine/Interfaces/AStarFramework.java
@@ -0,0 +1,21 @@
+/*------------------------------------------------------------------------------
+ @Author Art Garcia
+ Copyright (c) 2/2020
+ Project: AStarEngine
+ Last Modified: 2/25/20, 4:44 PM
+ License: GNU Lesser General Public License v3.0
+ -----------------------------------------------------------------------------*/
+
+package Engine.Interfaces;
+
+import Engine.Abstracts.AbstractNode;
+import Engine.Node;
+
+import java.util.List;
+
+public interface AStarFramework {
+	double getHeuristic(AbstractNode node);
+	double getF(AbstractNode node, double gScore);
+	double getDistance(AbstractNode node1, AbstractNode node2);
+	List<AbstractNode> getNeighbors(AbstractNode node);
+}
