54812a6b101193fcf70be72e06fa72a59a67baa7
artg.dev <artg3.dev@gmail.com>
2020-02-28 14:27:48 -0500

    Create DrawingUI.java

---
+++ src/main/java/Paint/DrawingBoard/UI/DrawingUI.java
@@ -0,0 +1,15 @@
+/*------------------------------------------------------------------------------
+ @Author Art Garcia
+ Copyright (c) 2/2020
+ Project: AStarEngine
+ Last Modified: 2/28/20, 2:27 PM
+ License: GNU Lesser General Public License v3.0
+ -----------------------------------------------------------------------------*/
+
+package Paint.DrawingBoard.UI;
+
+import javax.swing.JPanel;
+
+public class DrawingUI extends JPanel {
+	
+}
